9c89d3be56d586cf6766ef4d7a013832a639c5e0
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-12-10 12:00:01 +0530

    auth changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     // Set token from Bearer token in header
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1]
     // Set token from cookie
   }
   else if (req.cookies.token) {
